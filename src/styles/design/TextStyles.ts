f906cda3dac2a0764b5f3cbe4f69c7c8b9c4d65f
Martin Heidegger <martin.heidegger@gmail.com>
2020-09-11 13:02:50 +0900

    refactored system now knows how to support base style that others can be derived from. ^_^

--- src/styles/design/TextStyles.ts
+++ src/styles/design/TextStyles.ts
@@ -3,159 +3,135 @@ import { Color } from './Color'
 import { Font } from './Font'
 import { TextStyle } from 'react-native'
 
-export const EnMain: TextStyle = Object.freeze({
+export const En: TextStyle = {
+  color: Color.dark,
+  fontFamily: Font.RobotoRegular,
+  fontSize: 33,
+  lineHeight: 33,
+  textAlign: 'left',
+  textTransform: 'none',
+  textAlignVertical: 'center'
+}
+
+export const EnBox: TextStyle = {
+  ...En,
+  color: Color.text,
+  fontSize: 24,
+  lineHeight: 24,
+  textAlign: 'center',
+  textAlignVertical: 'bottom'
+}
+
+export const EnButton: TextStyle = {
+  ...En,
+  color: Color.white,
+  textAlign: 'center',
+  textAlignVertical: 'top'
+}
+
+export const EnInput: TextStyle = {
+  ...En,
+  textAlignVertical: 'top'
+}
+
+export const EnItem: TextStyle = {
+  ...En,
+  color: Color.text,
+  fontSize: 55,
+  lineHeight: 55
+}
+
+export const EnLabel: TextStyle = En
+
+export const EnLongText: TextStyle = {
+  ...En,
+  color: Color.text,
+  fontSize: 31,
+  lineHeight: 31,
+  textAlignVertical: 'top'
+}
+
+export const EnMain: TextStyle = {
+  ...En,
   color: Color.text,
   fontFamily: Font.KellySlabRegular,
   fontSize: 95,
-  lineHeight: 115,
-  textAlign: 'right',
-  textTransform: 'none',
-  textAlignVertical: 'center'
-})
+  lineHeight: 95,
+  textAlign: 'right'
+}
 
-export const EnItem: TextStyle = Object.freeze({
-  color: Color.text,
-  fontFamily: Font.RobotoRegular,
-  fontSize: 55,
-  lineHeight: 64,
-  textAlign: 'left',
-  textTransform: 'none',
-  textAlignVertical: 'center'
-})
-
-export const EnLabel: TextStyle = Object.freeze({
-  color: Color.dark,
-  fontFamily: Font.RobotoRegular,
-  fontSize: 33,
-  lineHeight: 39,
-  textAlign: 'left',
-  textTransform: 'none',
-  textAlignVertical: 'center'
-})
-
-export const EnButton: TextStyle = Object.freeze({
-  color: Color.white,
-  fontFamily: Font.RobotoRegular,
-  fontSize: 33,
-  lineHeight: 39,
-  textAlign: 'center',
-  textTransform: 'none',
-  textAlignVertical: 'top'
-})
-
-export const EnInput: TextStyle = Object.freeze({
-  color: Color.dark,
-  fontFamily: Font.RobotoRegular,
-  fontSize: 33,
-  lineHeight: 39,
-  textAlign: 'left',
-  textTransform: 'none',
-  textAlignVertical: 'top'
-})
-
-export const EnLongText: TextStyle = Object.freeze({
-  color: Color.text,
-  fontFamily: Font.RobotoRegular,
-  fontSize: 31,
-  lineHeight: 37,
-  textAlign: 'left',
-  textTransform: 'none',
-  textAlignVertical: 'top'
-})
-
-export const EnBox: TextStyle = Object.freeze({
-  color: Color.text,
-  fontFamily: Font.RobotoRegular,
-  fontSize: 24,
-  lineHeight: 28,
-  textAlign: 'center',
-  textTransform: 'none',
-  textAlignVertical: 'bottom'
-})
-
-export const JaMain: TextStyle = Object.freeze({
-  color: Color.text,
-  fontFamily: Font.NotoSansJPRegular,
-  fontSize: 95,
-  lineHeight: 140,
-  textAlign: 'right',
-  textTransform: 'none',
-  textAlignVertical: 'center'
-})
-
-export const JaInput: TextStyle = Object.freeze({
-  color: Color.dark,
-  fontFamily: Font.NotoSansJPRegular,
-  fontSize: 29,
-  lineHeight: 43,
-  textAlign: 'left',
-  textTransform: 'none',
-  textAlignVertical: 'top'
-})
-
-export const JaLabel: TextStyle = Object.freeze({
+export const Ja: TextStyle = {
   color: Color.dark,
   fontFamily: Font.NotoSansJPRegular,
   fontSize: 33,
-  lineHeight: 49,
+  lineHeight: 33,
   textAlign: 'left',
   textTransform: 'none',
   textAlignVertical: 'center'
-})
+}
 
-export const JaItem: TextStyle = Object.freeze({
+export const JaBox: TextStyle = {
+  ...Ja,
   color: Color.text,
-  fontFamily: Font.NotoSansJPRegular,
-  fontSize: 45,
-  lineHeight: 66,
-  textAlign: 'left',
-  textTransform: 'none',
-  textAlignVertical: 'top'
-})
-
-export const JaButton: TextStyle = Object.freeze({
-  color: Color.white,
-  fontFamily: Font.NotoSansJPRegular,
-  fontSize: 33,
-  lineHeight: 49,
-  textAlign: 'center',
-  textTransform: 'none',
-  textAlignVertical: 'top'
-})
-
-export const JaLongText: TextStyle = Object.freeze({
-  color: Color.text,
-  fontFamily: Font.NotoSansJPRegular,
-  fontSize: 31,
-  lineHeight: 46,
-  textAlign: 'left',
-  textTransform: 'none',
-  textAlignVertical: 'top'
-})
-
-export const JaBox: TextStyle = Object.freeze({
-  color: Color.text,
-  fontFamily: Font.NotoSansJPRegular,
   fontSize: 19,
-  lineHeight: 28,
-  textAlign: 'center',
-  textTransform: 'none',
-  textAlignVertical: 'center'
-})
+  lineHeight: 19,
+  textAlign: 'center'
+}
 
+export const JaButton: TextStyle = {
+  ...Ja,
+  color: Color.white,
+  textAlign: 'center',
+  textAlignVertical: 'top'
+}
+
+export const JaInput: TextStyle = {
+  ...Ja,
+  fontSize: 29,
+  lineHeight: 29,
+  textAlignVertical: 'top'
+}
+
+export const JaItem: TextStyle = {
+  ...Ja,
+  color: Color.text,
+  fontSize: 45,
+  lineHeight: 45,
+  textAlignVertical: 'top'
+}
+
+export const JaLabel: TextStyle = Ja
+
+export const JaLongText: TextStyle = {
+  ...Ja,
+  color: Color.text,
+  fontSize: 31,
+  lineHeight: 31,
+  textAlignVertical: 'top'
+}
+
+export const JaMain: TextStyle = {
+  ...Ja,
+  color: Color.text,
+  fontSize: 95,
+  lineHeight: 95,
+  textAlign: 'right'
+}
 export const TextStyles = {
-  EnMain,
-  EnItem,
-  EnLabel,
+  En,
+  EnBox,
   EnButton,
   EnInput,
+  EnItem,
+  EnLabel,
   EnLongText,
-  EnBox,
-  JaMain,
-  JaInput,
-  JaLabel,
-  JaItem,
+  EnMain,
+  Ja,
+  JaBox,
   JaButton,
+  JaInput,
+  JaItem,
+  JaLabel,
   JaLongText,
-  JaBox
+  JaMain
 }
